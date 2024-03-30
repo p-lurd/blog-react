8bb905514f47276a53c611145a5cf6b848eb633d
Jedidiah <admin@Jedidiahs-MacBook-Pro.local>
2024-03-30 23:34:00 +0100

    useState

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,11 @@
 const Home = () => {
+    const greet = (name) => {
+        console.log(`Hello ${name}`)
+    }
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={()=>{greet("Tolu")}}>click</button>
         </div>
     );
 }
