e0f1f8b2d83ef32dde1fd56c5974178d99afd4c1
Jedidiah <admin@Jedidiahs-MacBook-Pro.local>
2024-03-31 22:53:34 +0100

    props

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,20 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
-    const greet = (name) => {
-        console.log(`Hello ${name}`)
-    }
-    return (
-        <div className="home">
-            <h2>Homepage</h2>
-            <button onClick={()=>{greet("Tolu")}}>click</button>
-        </div>
-    );
-}
- 
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
+  return <div className="home">
+    <BlogList blogs ={blogs}/>
+  </div>;
+};
+
 export default Home;
