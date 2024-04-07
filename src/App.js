a3cae0507e529ebecb13abb2328078077bcafb5a
Jedidiah <admin@Jedidiahs-MacBook-Pro.local>
2024-04-07 19:19:58 +0100

    route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
 import Navbar from './navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Navbar />
       <div className="content">
-        <Home/>
+        <Switch>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
-  useFetch("http://localhost:8000/blogs")
+  const {data: blogs, error, isPending } = useFetch("http://localhost:8000/blogs")
   
   return <div className="home">
      { error && <div>{ error }</div> }
