1c86be3153e5e9cfa053e34e49bb7087c3b10975
Jedidiah <admin@Jedidiahs-MacBook-Pro.local>
2024-04-07 17:08:46 +0100

    custom hooks

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <Navbar />
       <div className="content">
-        <Home></Home>
+        <Home/>
       </div>
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,11 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]); // blog useState should initially be null so as to allow fetching
-
-  const [isPending, setIsPending] = useState(true);
-
-  useEffect(() => {
-      fetch('blogs') // use your api end point instead
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        setIsPending(false);
-        setBlogs(data);
-      })
-  }, [])
+  useFetch("http://localhost:8000/blogs")
+  
   return <div className="home">
+     { error && <div>{ error }</div> }
      { isPending && <div>Loading...</div> }
      {blogs && <BlogList blogs={blogs} />}
   </div>;
